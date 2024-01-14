753a9c1c7a3d8ddebf70424f595028e1c79d479c
Your Name <you@example.com>
2024-01-14 16:16:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
+// Challenge 1: Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 // Write your code here
+// Challenge 2: Random
+let random = Math.floor(Math.random() * 10) + 1; 
+// Generates a random integer between 1 and 10
+// Challenge 3: Mod
+let num3 = 40;
+let num4 = 6;
+let mod = num3 % num4;
+
+// Challenge 4: Max
+let max = Math.max(2, 5, 8, 15, 20);
+
+// Exporting variables for testing
+module.exports = { multiply, random, mod, max };
